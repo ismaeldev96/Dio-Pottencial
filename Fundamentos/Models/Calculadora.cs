146b65fd2498dc463dbb4387842b3fd540f9ab01
Ismael <ismaeldev96@gmail.com>
2022-09-25 19:12:20 -0300

    Incrementando a Funação de potencia na calculadora usando o MATH

--- Fundamentos/Models/Calculadora.cs
+++ Fundamentos/Models/Calculadora.cs
@@ -23,6 +23,9 @@ namespace Fundamentos.Models
         {
             Console.WriteLine($"{x} / {y} = {x / y}");
         }
-
+        public void Potencia(int x, int y){
+            double pot = Math.Pow(x, y);
+            Console.WriteLine($"{x} ^ {y} = {pot}");
+        }
     }
 }
