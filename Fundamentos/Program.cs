3a0436025bea8ec7c47890263fb1b266a32258dd
Ismael <ismaeldev96@gmail.com>
2022-09-25 13:53:12 -0300

    Aprendendo sobre alguns tipos de dados e sobre a conversão dos tipos de variaveis

--- Fundamentos/Program.cs
+++ Fundamentos/Program.cs
@@ -1,8 +1,22 @@
 ﻿using Fundamentos.Models;
 
-Pessoa p = new Pessoa();
+// // TIPOS DE DADOS
+// string nome = "ismael"; // a string sempre atribuida pelo valor entre aspas 
+// Console.WriteLine($"Meu nome é {nome}");
 
-p.Nome = "Ismael";
-p.Idade = 26;
+// int idade = 26; // tipo numerico para inteiro 
+// Console.WriteLine($"Tenho {idade}");
 
-p.Apresentar();
+// decimal salario = 10000.00M; // o tipo numerico Decimal é recomendado para usar em valores monetarios
+// Console.WriteLine($"Recebo {salario.ToString("c")}"); // ToString converte para uma saida em formato de string o "C" é para colocar no formato de valor monetario em reais
+
+// DateTime dataatual = DateTime.Now; //DateTime é uma classe resevada para o tipo de dados de data .Now pega sempre á data atual pelo sistema da maquina que esta rodando o programa
+// Console.WriteLine(dataatual);
+
+// INSTANCIANDO UMA CLASSE 
+// Pessoa p = new Pessoa();
+
+// p.Nome = "Ismael";
+// p.Idade = 26;
+
+// p.Apresentar();
